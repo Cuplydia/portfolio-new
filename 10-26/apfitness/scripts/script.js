005dd44fe1eaeeb418721690a6e0ec27efc529ed
Cuplydia <neneperkins08gmail.com>
2024-06-09 15:44:56 -0500

    My first commit

---
+++ 10-26/apfitness/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Anita Perkins
+    File Name: forward fitness club script.js
+    Date: March 14, 2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});    
